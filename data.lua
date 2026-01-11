0e429a38de492440e297a332b6e4da38da63749a
Peter Wolf <84736182+peterwolf-pl@users.noreply.github.com>
2026-01-11 06:32:12 +0100

    Fix collision mask format

--- data.lua
+++ data.lua
@@ -8,7 +8,7 @@ lamp.placeable_by = nil
 lamp.flags = {"placeable-off-grid", "not-on-map", "hidden"}
 lamp.selection_box = {{0, 0}, {0, 0}}
 lamp.collision_box = {{0, 0}, {0, 0}}
-lamp.collision_mask = {}
+lamp.collision_mask = {layers = {}}
 lamp.selectable_in_game = false
 lamp.energy_usage_per_tick = "1KW"
 lamp.light = {intensity = 0.6, size = 12, color = {r = 1, g = 0.95, b = 0.85}}
